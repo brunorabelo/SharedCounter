a9b46f50356b02aca1cc4a6338e19b4476479f64
Bruno Botelho <brunorabelow@gmail.com>
2021-11-16 18:28:15 +0100

    tests

--- appcounter/settings.py
+++ appcounter/settings.py
@@ -68,8 +68,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'appcounter.wsgi.application'
-ASGI_APPLICATION = "appcounter.asgi.application"
 
+# Channels
+ASGI_APPLICATION = "appcounter.asgi.application"
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
