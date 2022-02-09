3c9b34d9aa171fd8615c9c79d18d84290341652e
Bruno Botelho <brunorabelow@gmail.com>
2022-02-09 20:41:37 +0100

    deactivate lint

--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
 from django.conf import settings
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 import uuid
 # Create your views here.
 from django.shortcuts import render
 from django.urls import reverse
 import redis
 
-
 redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
                                    port=settings.REDIS_PORT, db=0)
+
+
 def index(request):
     return render(request, 'core/index.html')
 
@@ -24,4 +25,3 @@ def room(request, room_name):
 def create_room(request):
     room_name = uuid.uuid4().hex[:6].upper()
     return redirect(reverse('room', kwargs={'room_name': room_name}))
-
