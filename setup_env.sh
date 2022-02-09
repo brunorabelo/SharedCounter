edf7dce11563cbca7272f9db7073e818de34bb85
Bruno Botelho <brunorabelow@gmail.com>
2022-02-09 20:28:07 +0100

    setup env

--- setup_env.sh
+++ setup_env.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # shellcheck disable=SC2129
-echo DEBUG=0 >> /docker/config/.env.prod
+echo DEBUG=0 >> .env
 
-echo SQL_ENGINE=$SQL_ENGINE>> /docker/config/.env.prod
-echo DATABASE=$DATABASE >> /docker/config/.env.prod
-echo SECRET_KEY=$SECRET_KEY >> /docker/config/.env.prod
-echo DJANGO_ALLOWED_HOSTS=$SECRET_KEY >> /docker/config/.env.prod
-echo SQL_DATABASE=$SQL_DATABASE >> /docker/config/.env.prod
-echo SQL_USER=$SQL_USER >> /docker/config/.env.prod
-echo SQL_PASSWORD=$SQL_PASSWORD >> /docker/config/.env.prod
-echo SQL_HOST=$SQL_HOST >> /docker/config/.env.prod
-echo SQL_PORT=$SQL_PORT >> /docker/config/.env.prod
-echo REDIS_HOST=$REDIS_HOST >> /docker/config/.env.prod
-echo REDIS_PORT=$REDIS_PORT >> /docker/config/.env.prod
-echo WEB_IMAGE=$IMAGE:web  >> /docker/config/.env.prod
-echo NGINX_IMAGE=$IMAGE:nginx  >> /docker/config/.env.prod
-echo CI_REGISTRY_USER=$CI_REGISTRY_USER   >> /docker/config/.env.prod
-echo CI_JOB_TOKEN=$CI_JOB_TOKEN  >> /docker/config/.env.prod
-echo CI_REGISTRY=$CI_REGISTRY  >> /docker/config/.env.prod
-echo IMAGE=$CI_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME >> /docker/config/.env.prod
-echo SERVER_IP=$SERVER_IP >> /docker/config/.env.prod
-echo SERVER_USER=$SERVER_USER >> /docker/config/.env.prod
+echo SQL_ENGINE=$SQL_ENGINE>> .env
+echo DATABASE=$DATABASE >> .env
+echo SECRET_KEY=$SECRET_KEY >> .env
+echo DJANGO_ALLOWED_HOSTS=$SECRET_KEY >> .env
+echo SQL_DATABASE=$SQL_DATABASE >> .env
+echo SQL_USER=$SQL_USER >> .env
+echo SQL_PASSWORD=$SQL_PASSWORD >> .env
+echo SQL_HOST=$SQL_HOST >> .env
+echo SQL_PORT=$SQL_PORT >> .env
+echo REDIS_HOST=$REDIS_HOST >> .env
+echo REDIS_PORT=$REDIS_PORT >> .env
+echo WEB_IMAGE=$IMAGE:web  >> .env
+echo NGINX_IMAGE=$IMAGE:nginx  >> .env
+echo CI_REGISTRY_USER=$CI_REGISTRY_USER   >> .env
+echo CI_JOB_TOKEN=$CI_JOB_TOKEN  >> .env
+echo CI_REGISTRY=$CI_REGISTRY  >> .env
+echo IMAGE=$CI_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME >> .env
+echo SERVER_IP=$SERVER_IP >> .env
+echo SERVER_USER=$SERVER_USER >> .env
 
 
