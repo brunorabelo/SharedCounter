9405f90ad7f204de83bd7a36153de6a657181411
Bruno Botelho <brunorabelow@gmail.com>
2022-02-10 18:46:23 +0100

    consumers mais organizado

--- core/consumers.py
+++ core/consumers.py
@@ -1,62 +1,76 @@
 # chat/consumers.py
 import json
-#from urllib import request
 from channels.generic.websocket import AsyncWebsocketConsumer
-import redis
-from django.conf import settings
-from django.http import HttpRequest
+
+from core.services import redis_service
 
 
 class CounterConsumer(AsyncWebsocketConsumer):
-    redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
-                                       port=settings.REDIS_PORT, db=0)
+    def __init__(self):
+        self.room_group_name = ''
 
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'counter_{self.room_name}'
-
-        # Join room group
+        room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'counter_{room_name}'
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        print(text_data)
-        # current_client = text_data_json['total']
-        self.redis_instance.incr(self.room_group_name)
+    async def receive(self, content):
+        '''
+        :param content: ex.: {'event': 'count.inc' ou 'user.joined', 'user.left', 'data':  }
+        :return:
+        '''
+        data = json.loads(content)
+        command_type = data.get('event')
 
-        current = int(self.redis_instance.get(self.room_group_name) or 0)
+        if command_type == 'count.inc':
+            await self._count_inc(data)
+        elif command_type == 'user.joined':
+            await self._user_joined(data)
+        elif command_type == 'user.left':
+            await self._user_left(data)
 
-        # Send message to room group
+    # Receive message from room group
+    async def echo_data(self, data):
+        # Send message to WebSocket
+        await self.send_json(data)
+
+    async def _count_inc(self, data):
+        current_count = self._count_inc(self.room_group_name)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'counter_inc',
-                'total': current
+                'type': 'echo.data',
+                'data': {
+                    'counter_total': current_count
+                }
             }
         )
 
-    async def counter_reset(self, event):
-        pass
+    def _user_joined(self, data):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'echo.data',
+                'data': data
+            }
+        )
 
-    # Receive message from room group
-    async def counter_inc(self, event):
-        print(event)
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'type': 'counter_inc',
-            'total': event['total']
-        }))
+    def _user_left(self, data):
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'echo.data',
+                'data': data
+            }
+        )
--- core/services/redis_service.py
+++ core/services/redis_service.py
@@ -6,6 +6,13 @@ redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
 
 
 def get_room_count(room_name):
-    room_group_name = 'counter_%s' % room_name
+    room_group_name = room_name if room_name.startswith('counter_') else f'counter_{room_name}'
     count = int(redis_instance.get(room_group_name) or 0)
     return count
+
+
+def inc_room_counter(room_name):
+    redis_instance.incr(room_name)
+    count = int(redis_instance.get(room_name) or 0)
+    return count
+
