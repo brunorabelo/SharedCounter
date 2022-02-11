65d802dff0cb840206e2e9c5bd71d8b41220312f
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 17:08:27 +0100

    deploy

--- docker/bin/deploy.sh
+++ docker/bin/deploy.sh
@@ -2,7 +2,6 @@
 
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
   cd /home/ubuntu/app
-  docker-compose -f docker-compose.prod.yml down && true
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:web
