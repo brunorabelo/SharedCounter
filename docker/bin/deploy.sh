939395524400c3cd001cad9ff700d64c2609cd46
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 20:12:16 +0100

    deploy5

--- docker/bin/deploy.sh
+++ docker/bin/deploy.sh
@@ -6,5 +6,5 @@ ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $TAG_COMMIT
   docker container rm -f appcounter || true
-  docker-run -d -p 8000:8000 -p 8001:8001 -v static_volume:/home/app/web/staticfiles --env-file=.env --name appcounter $TAG_COMMIT
+  docker run -d -p 8000:8000 -p 8001:8001 -v static_volume:/home/app/web/staticfiles --env-file=.env --name appcounter $TAG_COMMIT
 ENDSSH
