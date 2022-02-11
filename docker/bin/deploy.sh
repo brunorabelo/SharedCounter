418437e711a644b0a433447b1ea4cd1dfb2b6f4b
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 22:36:20 +0100

    fix yml6

--- docker/bin/deploy.sh
+++ docker/bin/deploy.sh
@@ -4,8 +4,8 @@ ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
   cd /home/ubuntu/app
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $RELEASE_WEB_IMAGE
-  docker pull $RELEASE_NGINX_IMAGE
+  docker pull $COMMIT_WEB_IMAGE_TAG
+  docker pull $COMMIT_NGINX_IMAGE_TAG
   docker-compose -f docker-compose.prod.yml down
   docker-compose -f docker-compose.prod.yml build
   docker-compose -f docker-compose.prod.yml up -d
