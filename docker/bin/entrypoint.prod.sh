afbc4c13935b9a7bdd83ffda83d44c49bd4fc19f
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 23:25:06 +0100

    final

--- docker/bin/entrypoint.prod.sh
+++ docker/bin/entrypoint.prod.sh
@@ -4,6 +4,4 @@ python manage.py collectstatic --no-input --clear
 
 daphne -p 8001 appcounter.asgi:application -b 0.0.0.0 &
 
-gunicorn appcounter.wsgi:application --bind 0.0.0.0:8000 &
-
 exec "$@"
