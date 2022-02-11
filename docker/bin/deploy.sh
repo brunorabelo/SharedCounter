25add59dc82a0f4c832ae9f47f6b3939b824456a
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 17:19:41 +0100

    trying push

--- docker/bin/deploy.sh
+++ docker/bin/deploy.sh
@@ -2,9 +2,11 @@
 
 ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
   cd /home/ubuntu/app
+  docker-compose -f docker-compose.prod.yml down && true
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx
-  docker-compose -f docker-compose.prod.yml up -d  --force-recreate
+  docker-compose -f docker-compose.prod.yml build
+  docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
