96c3132f0f09e7bfa8533127f8d8ab06eabfa455
Bruno Botelho <brunorabelow@gmail.com>
2022-02-11 22:25:29 +0100

    docker tags

--- docker/bin/deploy.sh
+++ docker/bin/deploy.sh
@@ -4,7 +4,9 @@ ssh -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << 'ENDSSH'
   cd /home/ubuntu/app
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  docker pull $TAG_COMMIT
-  docker container rm -f appcounter || true
-  docker run -d -p 8000:8000 -p 8001:8001 -v static_volume:/home/app/web/staticfiles --env-file=.env --name appcounter $TAG_COMMIT
+  docker pull $RELEASE_WEB_IMAGE
+  docker pull $RELEASE_NGINX_IMAGE
+  docker-compose -f docker-compose.prod.yml down
+  docker-compose -f docker-compose.prod.yml build
+  docker-compose -f docker-compose.prod.yml up -d
 ENDSSH
