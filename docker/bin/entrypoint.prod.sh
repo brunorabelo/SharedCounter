a3f415378c3d1618e19515dc3d5d3aaefee04034
Bruno Botelho <brunorabelow@gmail.com>
2022-02-14 20:54:01 +0100

    daphne

--- docker/bin/entrypoint.prod.sh
+++ docker/bin/entrypoint.prod.sh
@@ -2,6 +2,6 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input --clear
 
-#daphne -p 8001 appcounter.asgi:application -b 0.0.0.0 &
+daphne -p 8001 appcounter.asgi:application -b 0.0.0.0 &
 
 exec "$@"
