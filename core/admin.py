959b641a0878a1f8fb30da479a9b57a37814a53a
Bruno Botelho <brunorabelow@gmail.com>
2022-02-15 18:37:57 +0100

    Admin com filtro e melhor visao

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Room, Connection
 
+
 # Register your models here.
+@admin.register(Connection)
+class ConnectionAdmin(admin.ModelAdmin):
+    list_display = ("user", "room", "alive")
+    list_filter = ("room", "alive")
+
 
 admin.site.register(Room)
-admin.site.register(Connection)
